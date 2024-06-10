decd15d18f915997addece0eb860412bdfaada7b
louisnw <louisnw@icloud.com>
2024-06-10 14:48:45 +0100

    set up expo framework

---
+++ apps/smart-hydration-app/app/index.tsx
@@ -0,0 +1,10 @@
+import "../global.css"
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Text className="bg-black text-md text-white">Hello</Text>
+        </View>
+    );
+}
