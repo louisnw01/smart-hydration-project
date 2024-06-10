2b67d721f264cfbae9b314a8fc5e3be017ad6dcc
louisnw <louisnw@icloud.com>
2024-06-10 15:05:16 +0100

    fix expo

--- smart-hydration-app/app/index.tsx
+++ smart-hydration-app/app/index.tsx
@@ -1,9 +1,9 @@
 import "../global.css"
 import { Text, View } from 'react-native';
 
-export default function HomeScreen() {
+export default function Index() {
     return (
-        <View>
+        <View className="flex align-center justify-center h-screen">
             <Text className="bg-black text-md text-white">Hello</Text>
         </View>
     );
