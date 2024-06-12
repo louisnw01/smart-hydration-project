9471cbad39f5f065a161640d4e32889b82e595c7
Hamish Partington <hamishpartington25@gmail.com>
2024-06-12 15:15:22 +0100

    Simple hydration status placeholder

--- smart-hydration-app/app/home.tsx
+++ smart-hydration-app/app/home.tsx
@@ -3,6 +3,7 @@ import Drop from "../assets/svgs/water-drop-svgrepo-com.svg"
 import PageWrapper from "@/components/common/page-wrapper";
 import PageHeading from "@/components/common/page-heading";
 import HydrationPercentage from "@/components/hydration-pct";
+import HydrationStatus from "@/components/hydration-status";
 
 export default function HomePage() {
     return (
@@ -12,6 +13,7 @@ export default function HomePage() {
         <View className="flex flex-row justify-center mt-32">
             <Drop width={200} height={200}/>
         </View>
+        <HydrationStatus/>
     </PageWrapper>
     )
 }
