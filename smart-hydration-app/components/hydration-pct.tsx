3262968ec5c34ae22f85eabf40093853118cc7ab
Hamish Partington <hamishpartington25@gmail.com>
2024-06-12 14:31:08 +0100

    Simple placeholder for hydration percentage

---
+++ smart-hydration-app/components/hydration-pct.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+export default function HydrationPercentage() {
+    return (
+            <Text className= "w-full text-center text-8xl mt-16">
+                70%
+            </Text>
+    )
+}
