3d6263ec48ca1e43d1cce97239978eb6df789f54
Hamish Partington <hamishpartington25@gmail.com>
2024-06-12 15:57:53 +0100

    Basic status logic

--- smart-hydration-app/components/hydration-pct.tsx
+++ smart-hydration-app/components/hydration-pct.tsx
@@ -1,9 +1,13 @@
 import { Text } from "react-native";
+import { hydrationAtom } from "@/atom/hydration";
+import { useAtomValue } from "jotai";
 
 export default function HydrationPercentage() {
+const hydration = useAtomValue(hydrationAtom);
+
     return (
             <Text className= "w-full text-center text-8xl mt-16">
-                70%
+                {hydration.toString()}%
             </Text>
     )
 }
--- smart-hydration-app/components/hydration-status.tsx
+++ smart-hydration-app/components/hydration-status.tsx
@@ -1,10 +1,20 @@
 import { View, Text } from "react-native";
+import { hydrationAtom } from "@/atom/hydration";
+import { useAtomValue } from "jotai";
+import { HYDRATION_MESSAGE } from "@/constants/hydration-status";
+
 
 export default function HydrationStatus () {
+    const hydration = useAtomValue(hydrationAtom);
+    const hydrated = hydration >= 70;
+
     return (
-        <View className="mx-6 px-7 bg-gray-200 py-4 flex flex-row rounded-xl mt-16">
-            <Text className="w-full text-center text-3xl" >
-                Well done! You're on track!
+        <View className="mx-6 px-7 bg-gray-200 py-4 flex flex-row rounded-xl mt-16"
+        style = {{
+            backgroundColor: hydrated ? "green" : "red"
+    }}>
+            <Text className="w-full text-center text-3xl text-white">
+                {hydrated ? HYDRATION_MESSAGE.high : HYDRATION_MESSAGE.low}
             </Text>
         </View>
     )
