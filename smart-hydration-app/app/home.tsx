59923f1ccecd5231f639a8919880a5a18bca7f67
Hamish Partington <hamishpartington25@gmail.com>
2024-06-12 14:31:08 +0100

    Simple placeholder for hydration percentage

--- smart-hydration-app/app/home.tsx
+++ smart-hydration-app/app/home.tsx
@@ -2,12 +2,13 @@ import { Text, View } from "react-native";
 import Drop from "../assets/svgs/water-drop-svgrepo-com.svg"
 import PageWrapper from "@/components/common/page-wrapper";
 import PageHeading from "@/components/common/page-heading";
+import HydrationPercentage from "@/components/hydration-pct";
 
 export default function HomePage() {
     return (
     <PageWrapper>
         <PageHeading text='home page' />
-
+        <HydrationPercentage/>
         <View className="flex flex-row justify-center mt-32">
             <Drop width={200} height={200}/>
         </View>
