ab9854f03dab12094a456ebc373384659ee2cfba
louisnw <louisnw@icloud.com>
2024-06-12 15:55:04 +0100

    setup blank onboarding

--- smart-hydration-app/app/index.tsx
+++ smart-hydration-app/app/index.tsx
@@ -4,20 +4,23 @@ import { Text, View } from 'react-native';
 import { useAtomValue } from "jotai";
 import { selectedPageAtom } from "@/atom/nav";
 import PageRouter from "@/components/page-router";
+import OnboardingPage from "./onboarding";
 
 export default function Index() {
     const page = useAtomValue(selectedPageAtom);
 
-    return (
+    const isOnboarding = true;
 
+    if (isOnboarding) {
+        return <OnboardingPage />
+    }
+
+    return (
         <View className="flex flex-1 justify-between h-full">
             {/*<Text className="bg-black text-md text-white">you are hydrated</Text>*/}
             <PageRouter />
             <NavigationBar />
-
-
         </View>
-
     );
 }
 
---
+++ smart-hydration-app/app/onboarding.tsx
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+
+
+export default function OnboardingPage() {
+    return (
+        <Text>onboarding entry point</Text>
+    )
+}
