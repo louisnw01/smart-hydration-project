30547f28705a54cbc1281490d2005761b789fdcd
louisnw <louisnw@icloud.com>
2024-06-15 19:45:07 +0100

    implement global colors for both tailwind and normal styling

--- smart-hydration-app/tailwind.config.js
+++ smart-hydration-app/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("./colors")
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    // NOTE: Update this to include the paths to all of your component files.
+    content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+    presets: [require("nativewind/preset")],
+    theme: {
+        extend: {
+            colors: require('./colors')
+        },
+    },
+    plugins: [],
+
 }
