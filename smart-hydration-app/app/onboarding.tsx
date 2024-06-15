156bf8694e9e775cf9c520940efc25e7a04712c5
chriskerrc <chriskerrc@gmail.com>
2024-06-15 02:17:09 +0100

    Rough onboarding pages with generic content component and next button

--- smart-hydration-app/app/onboarding.tsx
+++ smart-hydration-app/app/onboarding.tsx
@@ -1,8 +1,54 @@
-import { Text } from "react-native";
+
+import { View, Text } from "react-native";
+import { atom, useAtom } from "jotai";
+import GenericOnboardContent from "@/components/generic-onboard-content";
+import NextButton from "@/components/next-button";
+import SubmitButton from "@/components/submit-button";
 
 
-export default function OnboardingPage() {
-    return (
-        <Text>onboarding entry point</Text>
-    )
+export const currentPageAtom = atom('home');
+export const pageIndexAtom = atom(0);
+
+export default function OnboardingPage( {}){
+  const [pageIndex, setPageIndex] = useAtom(pageIndexAtom);
+  
+  //replace content with components e.g. TextInput with props for specific text like "Enter your name"
+  const pages = [
+    {
+      title: 'What is your name?',
+      content: <Text>Placeholder for name page content</Text>,
+    },
+    {
+      title: 'What is your age?',
+      content: <Text>Placeholder for age page content</Text>,
+    },
+    {
+      title: 'What is your height?',
+      content: <Text>Placeholder for height page content</Text>,
+    }
+  ]
+
+  const currentPageContent = pages[pageIndex];
+
+  const maxPageIndex = pages.length;
+
+  const handleNext = () => {
+    setPageIndex((prevIndex) => (prevIndex + 1) % pages.length);
+  };
+
+  const handleSubmit = () => {
+   //do nothing for now
+   //later, go to homepage
+  };
+
+  const CurrentPage = pages[pageIndex];
+  return (
+    <View>
+      <GenericOnboardContent title={currentPageContent.title} content={currentPageContent.content} />
+      {pageIndex < maxPageIndex - 1 && <NextButton onPress={handleNext} />}
+      {pageIndex === maxPageIndex - 1 && <SubmitButton onPress={handleSubmit} />}
+    </View>
+  )
 }
+
+//Don't know how to style the components properly
