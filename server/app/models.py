6120ce04da603c14eb303da963d5b6b4309d2d18
dk23586@bristol.ac.uk <jasminethakral@gmail.com>
2024-06-18 20:28:05 +0100

    initial finding user and deleting

--- server/app/models.py
+++ server/app/models.py
@@ -8,7 +8,7 @@ class User(db.Entity):
     id = PrimaryKey(int, auto=True)
     email = Required(str)
     name = Required(str)
-    community = Required('Community')
+    community = Optional('Community')
     hash = Required(str)
     jug_user = Optional('JugUser', reverse="user")
 
--- server/app/pony-test.py
+++ server/app/pony-test.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from models import db
-from services import find_user, create_user
+from services import find_user, create_user, delete_user
 
 
 load_dotenv()
@@ -14,3 +14,10 @@ db.bind(
     database='postgres'
 )
 db.generate_mapping(create_tables=True)
+
+create_user('jasmine','email','hash')
+#find_user('Neill')
+#delete_user('jasmine','email','hash')
+#how to make sure it works without community
+#is that info only stored in the user table
+#make sure how community works -> creating user what values can it have -> tried with integer but crashed
