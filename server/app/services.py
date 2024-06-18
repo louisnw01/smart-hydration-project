96fd3cae2f01d140a11d47d75ebfc1e76fa022ec
ijgs02 <isaacjgscott@gmail.com>
2024-06-18 19:19:51 +0100

    add find data about jug

--- server/app/services.py
+++ server/app/services.py
@@ -1,6 +1,9 @@
 import os
 from models import User, Jug
 from pony.orm.core import commit, select, db_session, set_sql_debug, show
+from api import login_and_get_session, headers
+import json
+import pprint
 
 
 @db_session
@@ -14,3 +17,11 @@ def find_user(name):
     result = select(u for u in User if u.name == name)
     show(result)
     return result
+
+@db_session
+def get_jug_data(sh_jug_id):
+    session = login_and_get_session()
+    response = session.get("https://www.smarthydration.online/data/device/" + sh_jug_id + "/events/hydration", headers=headers)
+    return response.json()
+
+pprint.pprint(get_jug_data("jug001052"))
