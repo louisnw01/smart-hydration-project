4320be2a7180ef21c809ace19d7827f5fbf1fdb4
dk23586@bristol.ac.uk <jasminethakral@gmail.com>
2024-06-19 15:44:35 +0100

    fixing bug in submit-button

--- smart-hydration-app/components/submit-button.tsx
+++ smart-hydration-app/components/submit-button.tsx
@@ -2,11 +2,8 @@ import {Text, Pressable, View} from "react-native";
 
 interface SubmitButtonProps {
     onPress: () => void;
-<<<<<<< HEAD
   }
-=======
-  }  
->>>>>>> 76493d7070a7c311f41169784180511ff99bf82f
+
 //later: refactor copy and pasting between Submit and Next
 export default function SubmitButton( { onPress }: SubmitButtonProps) {
     return (
