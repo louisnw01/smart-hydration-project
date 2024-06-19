127d4e6882e49774c474fbf04f65dd96f813e028
louisnw <louisnw@icloud.com>
2024-06-19 17:50:00 +0100

    implement login/registration

--- server/app/main.py
+++ server/app/main.py
@@ -1,7 +1,12 @@
 import os
-from fastapi import FastAPI
-from .models import db
+from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
+
+from .models import db
+from .schemas import UserLogin, UserRegister
+from .services import create_user, get_user_hash, get_auth_token, user_exists
+from .util import get_hash
+
 
 load_dotenv()
 
@@ -20,6 +25,31 @@ db.generate_mapping(create_tables=True)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/register")
+async def register(form: UserRegister):
+    if user_exists(form.email):
+        raise HTTPException(status_code=400, detail="email already registered")
+
+    hashed_password = get_hash(form.password)
+    create_user(form.name, form.email, hashed_password)
+    return {"message": "success"}
+
+
+@app.post("/login")
+async def login(form: UserLogin):
+    if not user_exists(form.email):
+        raise HTTPException(status_code=400, detail="incorrect email or password")
+
+    hashed_password = get_user_hash(form.email)
+    given_hash = get_hash(form.password)
+
+    if hashed_password != given_hash:
+        raise HTTPException(status_code=400, detail="incorrect email or password")
+
+    token = get_auth_token(form.email)
+    return {"access_token": token, "token_type": "bearer"}
 
 
 
