3ceb0535be5c6bf50e17ef3b254105f657708c52
dk23586@bristol.ac.uk <jasminethakral@gmail.com>
2024-06-19 15:33:11 +0100

    integrating into onboarding branch with jasmine's changes

--- smart-hydration-app/components/next-button.tsx
+++ smart-hydration-app/components/next-button.tsx
@@ -2,14 +2,12 @@ import {Text, Pressable, View} from "react-native";
 
 interface NextButtonProps {
     onPress: () => void;
-  }  
+  }
 
 export default function NextButton( { onPress }: NextButtonProps) {
     return (
-        <Pressable onPress={onPress}>
-            <View style = {{backgroundColor: "cyan",  alignSelf: 'flex-start' }}>
-            <Text>Next</Text>
-            </View>
+        <Pressable className="absolute bottom-0 right-0 m-5 bg-blue-200 rounded-lg px-2 py-1" onPress={onPress}>
+            <Text className="font-bold text-xl">Next</Text>
         </Pressable>
     )
 }
--- smart-hydration-app/components/submit-button.tsx
+++ smart-hydration-app/components/submit-button.tsx
@@ -2,14 +2,12 @@ import {Text, Pressable, View} from "react-native";
 
 interface SubmitButtonProps {
     onPress: () => void;
-  }  
-
+  }
+//later: refactor copy and pasting between Submit and Next
 export default function SubmitButton( { onPress }: SubmitButtonProps) {
     return (
-        <Pressable onPress={onPress}>
-            <View style = {{backgroundColor: "green",  alignSelf: 'flex-start' }}>
-            <Text>Submit</Text>
-            </View>
+        <Pressable className="absolute bottom-0 right-0 m-5 bg-green-200 rounded-lg px-2 py-1" onPress={onPress}>
+            <Text className="font-bold text-xl">Submit</Text>
         </Pressable>
     )
 }
