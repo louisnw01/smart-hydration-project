ca83ef0ac3092295d2fc8e204449f05c7be7ce11
Hamish Partington <hamishpartington25@gmail.com>
2024-06-21 11:46:09 +0100

    Functions to link and unlink a jug from a jug user

--- server/app/services.py
+++ server/app/services.py
@@ -1,11 +1,11 @@
 import os
-from models import User, Jug
+from models import User, Jug, JugUser, Community
 from pony.orm.core import commit, select, db_session, set_sql_debug, show
 
 
 @db_session
 def create_user(name, email, hash):
-    user = User(name=name, email=email, hash=hash)
+    User(name=name, email=email, hash=hash)
     commit()
 
 
@@ -14,3 +14,50 @@ def find_user(name):
     result = select(u for u in User if u.name == name)
     show(result)
     return result
+
+
+@db_session
+def create_jug_user(name, dob, community):
+    JugUser(name=name, dob=dob, community=community)
+    commit()
+
+
+@db_session
+def create_community(name):
+    Community(name=name)
+    commit()
+
+
+@db_session
+def get_community_id(name):
+    return Community.get(name=name).id
+
+
+@db_session
+def create_jug(sh_id, qr_hash, name):
+    Jug(smart_hydration_id=sh_id, qr_hash=qr_hash, name = name)
+    commit()
+
+
+@db_session
+def link_jug_to_jug_user(jug_id, jug_user_id):
+    jug = Jug.get(id=jug_id)
+    JugUser.get(id=jug_user_id).jugs.add(jug)
+    commit()
+
+
+@db_session
+def unlink_jug_from_jug_user(jug_id, jug_user_id):
+    jug = Jug.get(id=jug_id)
+    JugUser.get(id=jug_user_id).jugs.remove(jug)
+    commit()
+
+
+@db_session
+def get_jug_id(name):
+    return Jug.get(name=name).id
+
+
+@db_session
+def get_jug_user_id(name):
+    return JugUser.get(name=name).id
