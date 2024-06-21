07c090e4dfa7b7b617554ed5cb4e80b5e03921de
Hamish Partington <hamishpartington25@gmail.com>
2024-06-21 11:46:09 +0100

    Functions to link and unlink a jug from a jug user

--- server/app/pony-test.py
+++ server/app/pony-test.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from models import db
-from services import find_user, create_user
+from services import *
 
 
 load_dotenv()
@@ -14,3 +14,14 @@ db.bind(
     database='postgres'
 )
 db.generate_mapping(create_tables=True)
+
+
+# create_community("Jedi")
+# com_id = get_community_id("Jedi")
+# create_jug_user("Luke", "10103000", com_id)
+# create_jug("jug52", "hash66", "R2D2")
+jug_id = get_jug_id("R2D2")
+jug_user_id = get_jug_user_id("Luke")
+# link_jug_to_jug_user(jug_id, jug_user_id)
+unlink_jug_from_jug_user(jug_id, jug_user_id)
+
