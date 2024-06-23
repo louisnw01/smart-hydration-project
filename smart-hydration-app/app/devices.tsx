f73a3768599117d865324719773702561c58484a
louisnw <louisnw@icloud.com>
2024-06-23 13:06:55 +0100

    implement victory native charts

--- smart-hydration-app/app/devices.tsx
+++ smart-hydration-app/app/devices.tsx
@@ -7,15 +7,21 @@ import { useAtom } from "jotai";
 import { popupPageAtom } from "@/atom/nav";
 
 
-function DeviceRow({name}) {
+function DeviceRow({name, isStale, percentFull}) {
+
+
+    const staleness = isStale ? 'water is stale' : ''
+
     return (
         <View className="mx-6 bg-gray-200 px-7 py-4 flex flex-row justify-between rounded-xl">
             <View className="flex">
                 <Text className="text-xl font-bold">{name}</Text>
                 <Text className="">connected</Text>
             </View>
-            <View className='flex'>
-                <Text className="text-red-500 font-semibold text-right">water is stale</Text>
+            <View className='flex justify-evenly'>
+
+                <Text className="font-semibold text-right">{percentFull}% full</Text>
+                <Text className="font-semibold text-right" style={{color: 'red'}}>{staleness}</Text>
 
             </View>
         </View>
@@ -28,13 +34,13 @@ export default function DevicesPage() {
 
     return (
         <PageWrapper>
-            <PageHeading text="devices page">
+            <PageHeading text="Devices">
                 <Text className="text-3xl font-semibold" onPress={() => setPopup('devices')}>+</Text>
             </PageHeading>
 
             <View className="mt-16 flex gap-6">
-                <DeviceRow name="My Jug" />
-                <DeviceRow name="Isaac's Jug" />
+                <DeviceRow name="My Jug" isStale percentFull={43}/>
+                <DeviceRow name="Isaac's Jug" percentFull={23}/>
 
 
                 <View className="flex flex-row justify-center">
