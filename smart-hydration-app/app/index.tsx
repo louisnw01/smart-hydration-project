1e03a96da99500df6ff83a0705c579d1d5b5e57f
Hamish Partington <hamishpartington25@gmail.com>
2024-06-25 17:16:21 +0100

    jugs refresh after link/unlink

--- smart-hydration-app/app/index.tsx
+++ smart-hydration-app/app/index.tsx
@@ -3,15 +3,34 @@ import "../global.css"
 import { View } from 'react-native';
 import PageRouter from "@/components/page-router";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
+import { Provider } from 'jotai/react'
+import { useHydrateAtoms } from 'jotai/react/utils'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { queryClientAtom } from 'jotai-tanstack-query'
 
+const queryClient = new QueryClient()
+
+const HydrateAtoms = ({ children }) => {
+  useHydrateAtoms([[queryClientAtom, queryClient]])
+  return children
+}
 
 export default function Index() {
     return (
-        <GestureHandlerRootView>
-            <View className="flex flex-1 justify-between h-full">
-                <PageRouter />
-                <NavigationBar />
-            </View>
-        </GestureHandlerRootView>
+        <QueryClientProvider client={queryClient}>
+        <Provider>
+          <HydrateAtoms>
+            <GestureHandlerRootView>
+                <View className="flex flex-1 justify-between h-full">
+                    <PageRouter />
+                    <NavigationBar />
+                </View>
+            </GestureHandlerRootView>
+          </HydrateAtoms>
+        </Provider>
+      </QueryClientProvider>
     );
 }
