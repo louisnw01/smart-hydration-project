a3832d1742cb0571b1bdb2a231f7b7773ff0d17f
dk23586@bristol.ac.uk <jasminethakral@gmail.com>
2024-06-27 17:05:21 +0100

    getting rid of some conflicts

--- server/app/services.py
+++ server/app/services.py
@@ -11,8 +11,10 @@ def create_user(name, email, hash,community):
 @db_session
 def delete_user(user_name, email,hash):
     user = User.get(name=user_name,email=email,hash=hash)
+    #if user is found get community associated with user in user.community
     if user:
         community_id = user.community
+        #fetch community object using community's id community_id.id as community in USer model links to Community model 
         community = Community.get(id=community_id.id)
         community.delete()
         user.delete()
@@ -28,7 +30,7 @@ def find_user(name):
 def create_community(name):
     community = Community(name=name)
     commit()
-    return community 
+    return community
 
 @db_session
 def find_community(name):
