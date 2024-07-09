eb8e2ff4dec141e9ffdccd5864d0467d31cfb9e0
louisnw <louisnw@icloud.com>
2024-07-09 12:34:15 +0100

    trends page redesign, fix registration bug

---
+++ smart-hydration-app/components/common/water-amount.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+
+export default function WaterAmount({ value, unit }) {
+    unit = unit || "ml";
+    return (
+        <View className="flex flex-row items-end">
+            <Text className="font-bold text-4xl tabular-nums dark:text-white">
+                {value.toFixed(0)}
+            </Text>
+            <Text className="font-bold text-xl pb-[1px] dark:text-white">
+                {unit}
+            </Text>
+        </View>
+    );
+}
---
+++ smart-hydration-app/components/trends/today-vs-avg.tsx
@@ -0,0 +1,60 @@
+import {
+    amountDrankTodayAtom,
+    avgAmountDrankByTimeNowAtom,
+} from "@/util/trends";
+import { useAtomValue } from "jotai";
+import InsightsPane from "./insights-pane";
+import { View, Text } from "react-native";
+import WaterAmount from "../common/water-amount";
+
+export default function TodayVsAvgInsight() {
+    const amountDrankToday = useAtomValue(amountDrankTodayAtom);
+    const avgAmountDrankByNow = useAtomValue(avgAmountDrankByTimeNowAtom);
+
+    if (amountDrankToday == null || avgAmountDrankByNow == null) return null;
+
+    const dailyAvgDiff = amountDrankToday - avgAmountDrankByNow;
+
+    return (
+        <InsightsPane
+            heading={`So far today, you're drinking ${dailyAvgDiff > 0 ? "more" : "less"} than you normally would.`}
+        >
+            <View className="flex-row w-full gap-32">
+                <View className="flex">
+                    <Text className="font-bold dark:text-white">Today</Text>
+                    <WaterAmount value={amountDrankToday} />
+                </View>
+                <View className="flex">
+                    <Text className="font-bold dark:text-white">Average</Text>
+                    <WaterAmount value={avgAmountDrankByNow} />
+                </View>
+                {/* <Text
+                    style={{
+                        fontSize: 32,
+                        fontWeight: "bold",
+                        color: dailyAvgDiff > 0 ? "green" : "orange",
+                    }}
+                >
+                    {Math.abs(dailyAvgDiff).toFixed(0)}ml{" "}
+                </Text>
+                <Text
+                    style={{
+                        fontSize: 32,
+                        fontWeight: "bold",
+                        color: dailyAvgDiff > 0 ? "green" : "orange",
+                    }}
+                >
+                    {dailyAvgDiff > 0 ? "More" : "Less"}
+                </Text>
+                <Entypo
+                    className="py-2"
+                    name={
+                        dailyAvgDiff > 0 ? "arrow-long-up" : "arrow-long-down"
+                    }
+                    size={24}
+                    color={dailyAvgDiff > 0 ? "green" : "orange"}
+                /> */}
+            </View>
+        </InsightsPane>
+    );
+}
