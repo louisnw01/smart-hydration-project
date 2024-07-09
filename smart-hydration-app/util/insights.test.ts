0c906c3729e31828f10af49470dc2df0eddadf76
ijgs02 <isaacjgscott@gmail.com>
2024-07-09 10:54:26 +0100

    add tests

--- smart-hydration-app/util/insights.test.ts
+++ smart-hydration-app/util/insights.test.ts
@@ -1,5 +1,7 @@
-import { MS_DAY } from "@/constants/data";
+import {MS_DAY, MS_HOUR, MS_MONTH} from "@/constants/data";
 import {
+    averageHydrationMonthComparison,
+    getAmountDrankToday, getAvgAmountDrankByNow,
     getFloorOf,
     getMostHydratedDayOfWeek,
     getTodaysStartMS,
@@ -8,7 +10,6 @@ import {
 describe("insights", () => {
     test("highest day of week", () => {
         const todayMS = getFloorOf(new Date(2024, 7, 8).getTime(), MS_DAY);
-
         const aggs = [];
         for (let i = 0; i < 10; i++) {
             const newTS = todayMS - i * MS_DAY;
@@ -25,13 +26,80 @@ describe("insights", () => {
                 day: day,
             });
         }
-
         const { name, value } = getMostHydratedDayOfWeek(aggs);
-
         expect(name).toEqual("Monday");
 
         // an array of daily aggs
 
         // expect(val1).toEqual();
     });
+    test("get amount drank today", () => {
+        const aggs = [];
+        const todayStartMS = Math.floor(Date.now() / MS_DAY) * MS_DAY;
+
+        // create days with same value for average to come out nicely
+        for (let i = 0; i < 7; i++) {
+            const newTS = todayStartMS - i * MS_HOUR;
+            const day = new Date(newTS).getDay();
+            aggs.push({
+                time: todayStartMS + i * MS_HOUR,
+                amount: 500,
+            });
+
+        }
+        aggs[6].amount = 700;
+        console.log(aggs);
+        const amount = getAmountDrankToday(aggs);
+        expect(amount).toEqual(3700);
+    })
+    test("average amount of water to a certain point in the day", () => {
+        const aggs = [];
+        const todayStartMS = Math.floor(Date.now() / MS_DAY) * MS_DAY;
+        const yesterdayStartMS = (Math.floor(Date.now() / MS_DAY) * MS_DAY) - MS_DAY;
+
+        // create days with same value for average to come out nicely
+        for (let i = 0; i < 7; i++) {
+            const newTS = todayStartMS + i * MS_HOUR;
+            const day = new Date(newTS).getDay();
+            aggs.push({
+                time: todayStartMS + i * MS_HOUR,
+                value: 500,
+            });
+        }
+        for (let i = 0; i < 7; i++) {
+            const newTS = yesterdayStartMS + i * MS_HOUR;
+            const day = new Date(newTS).getDay();
+            aggs.push({
+                time: yesterdayStartMS + i * MS_HOUR,
+                value: 500,
+            });
+        }
+        const amount = getAvgAmountDrankByNow(aggs);
+        expect(amount).toEqual(500);
+    })
+    test("average hydration month comparison", () => {
+        const testData = [];
+        const thisMonthStart = Math.floor(Date.now() / MS_MONTH) * MS_MONTH;
+        const prevMonthStart = Math.floor(Date.now() / MS_MONTH) * MS_MONTH - MS_MONTH;
+
+        for (let i = 0; i < 31; i++) {
+            const newTS = thisMonthStart + i * MS_DAY;
+            const day = new Date(newTS).getDay();
+            testData.push({
+                x: thisMonthStart + i * MS_DAY,
+                y: 1500,
+            })
+        }
+        for (let i = 0; i < 29; i++) {
+            const newTS = prevMonthStart + i * MS_DAY;
+            const day = new Date(newTS).getDay();
+            testData.push({
+                x: prevMonthStart + i * MS_DAY,
+                y: 2000,
+            })
+        }
+        const [firstAvg, nextAvg] = averageHydrationMonthComparison(testData)
+        expect(firstAvg).toEqual(1500);
+        expect(nextAvg).toEqual(2000);
+    })
 });
