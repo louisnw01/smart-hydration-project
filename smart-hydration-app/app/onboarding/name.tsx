439562f708a80cbe08b2f920b75fa47ea8e7733d
Hamish Partington <hamishpartington25@gmail.com>
2024-07-10 11:54:30 +0100

    other input validations for onboarding

--- smart-hydration-app/app/onboarding/name.tsx
+++ smart-hydration-app/app/onboarding/name.tsx
@@ -1,24 +1,47 @@
 import { registerInfoAtom } from "@/atom/user";
 import GenericOnboardContent from "@/components/onboarding/generic-onboard-content";
 import { useSetAtom } from "jotai";
-import { TextInput } from "react-native";
+import { useState } from "react";
+import { TextInput, Text, View } from "react-native";
 
 export default function NamePage() {
+    const [errorMessage, setErrorMessage] = useState('');
+    const [proceed, setProceed] = useState(false);
+    const [name, setName] = useState('');
     const setInfo = useSetAtom(registerInfoAtom);
+
+    const validateName = () => {
+    
+        if (name.length == 0) {
+          setErrorMessage('You must enter your name.');
+          setProceed(false);
+        } else {
+          setErrorMessage('');
+          setProceed(true);
+          setInfo((prev) => ({ ...prev, name: name }))
+        }
+      };
+
     return (
         <GenericOnboardContent
             title="What is your name?"
             nextHref="onboarding/dob"
+            proceed={proceed}
         >
             <TextInput
-                placeholder="Enter your name"
-                onChangeText={(val) =>
-                    setInfo((prev) => ({ ...prev, name: val }))
-                }
+                placeholder="Enter your name (required)"
+                onChangeText={(val) => setName(val)}
                 className="bg-gray-200 w-full h-14 placeholder-black text-xl rounded-xl px-3"
                 textContentType="name"
                 returnKeyType="done"
+                onSubmitEditing={validateName}
+                onEndEditing={validateName}
             />
+            <View style={{ width: 350 }}>
+                <Text style={{ color: "red", fontSize: 18 }}>
+                    {errorMessage}
+                </Text>
+            </View>
         </GenericOnboardContent>
     );
 }
