420a4f8fe1cb63e6157761ff83b4f1cae011fe5e
Hamish Partington <hamishpartington25@gmail.com>
2024-07-11 19:33:50 +0100

    test

--- server/deploy.sh
+++ server/deploy.sh
@@ -5,10 +5,13 @@ SSH_SERVER="ec2-18-133-247-202.eu-west-2.compute.amazonaws.com"
 
 file=$1
 
+cat "$file"
+
 zip -r deploy.zip app/ Dockerfile Makefile requirements.txt .env
+
 
 scp -i "$file" deploy.zip ${SSH_USER}@${SSH_SERVER}:~
 
-ssh -i "$file" ${SSH_USER}@${SSH_SERVER} '/home/ec2-user/server_deploy.sh' | tee /dev/tty
+ssh -i "$file" ${SSH_USER}@${SSH_SERVER} '/home/ec2-user/server_deploy.sh'
 
 rm deploy.zip
