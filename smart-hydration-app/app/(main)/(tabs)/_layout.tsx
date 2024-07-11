ae03db7237ab49f123e805cc2a2ccd9fcddbcb87
louisnw <louisnw@icloud.com>
2024-07-11 17:52:30 +0100

    change droplet to fill screen, make chart labels clearer, fix test

--- smart-hydration-app/app/(main)/(tabs)/_layout.tsx
+++ smart-hydration-app/app/(main)/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import { hydrationInsightsEAtom } from "@/atom/effect/hydration";
 import PageHeader from "@/components/common/header";
+import { useWaterLevel } from "@/components/home/water-screen";
 import useColorPalette from "@/util/palette";
 import {
     Entypo,
@@ -12,6 +13,9 @@ import { useAtomValue } from "jotai";
 
 export default function TabLayout() {
     const palette = useColorPalette();
+    const waterLevel = useWaterLevel();
+    const underWater = waterLevel && waterLevel >= 0.1;
+    const cogUnderwater = waterLevel && waterLevel > 0.89;
     return (
         <Tabs
             screenOptions={{
@@ -26,6 +30,9 @@ export default function TabLayout() {
                     borderTopColor: palette.border,
                 },
                 tabBarActiveTintColor: palette.fg,
+                tabBarInactiveTintColor: underWater
+                    ? "white"
+                    : "rgb(145, 145, 145)",
             }}
             sceneContainerStyle={{
                 backgroundColor: palette.bg,
@@ -40,9 +47,20 @@ export default function TabLayout() {
                     ),
                     headerRight: () => (
                         <Link className="px-5" href="settings/settings-modal">
-                            <Entypo name="cog" size={30} color={palette.fg} />
+                            <Entypo
+                                name="cog"
+                                size={30}
+                                color={cogUnderwater ? "white" : palette.fg}
+                            />
                         </Link>
                     ),
+                    headerTransparent: true,
+                    tabBarStyle: {
+                        elevation: 0,
+                        backgroundColor: "transparent",
+                        position: "absolute",
+                        borderTopWidth: 0,
+                    },
                 }}
             />
             <Tabs.Screen
--- smart-hydration-app/util/insights.test.ts
+++ smart-hydration-app/util/insights.test.ts
@@ -1,14 +1,14 @@
-import {MS_DAY, MS_HOUR, MS_MONTH} from "@/constants/data";
+import { MS_DAY, MS_HOUR, MS_MONTH } from "@/constants/data";
 
 import {
     averageHydrationMonthComparison,
-    getAmountDrankToday, getAvgAmountDrankByNow,
+    getAmountDrankToday,
+    getAvgAmountDrankByNow,
     getFloorOf,
     getMostHydratedDayOfWeek,
     getTodaysStartMS,
 } from "./trends";
-import {any} from "prop-types";
-
+import { any } from "prop-types";
 
 describe("insights", () => {
     test("highest day of week", () => {
@@ -48,21 +48,21 @@ describe("insights", () => {
                 time: todayStartMS + i * MS_HOUR,
                 value: 500,
             });
-
         }
         aggs[6].value = 700;
         const amount = getAmountDrankToday(aggs);
         expect(amount).toEqual(3700);
-    })
+    });
     test("pass undefined to amount drank today", () => {
-        const testData = []
+        const testData = [];
         const result = getAmountDrankToday(testData);
         expect(result).toEqual(0);
-    })
+    });
     test("average amount of water to a certain point in the day", () => {
         const aggs = [];
         const todayStartMS = Math.floor(Date.now() / MS_DAY) * MS_DAY;
-        const yesterdayStartMS = (Math.floor(Date.now() / MS_DAY) * MS_DAY) - MS_DAY;
+        const yesterdayStartMS =
+            Math.floor(Date.now() / MS_DAY) * MS_DAY - MS_DAY;
 
         // create days with same value for average to come out nicely
         for (let i = 0; i < 7; i++) {
@@ -83,31 +83,16 @@ describe("insights", () => {
         }
         const amount = getAvgAmountDrankByNow(aggs);
         expect(amount).toEqual(500);
-    })
-    test('mixed weeks', () => {
-        const data = [
-            { time: new Date('2024-07-01T00:00:00Z').getTime(), value: 1000 },
-            { time: new Date('2024-07-02T00:00:00Z').getTime(), value: 1500 },
-            { time: new Date('2024-07-02T12:00:00Z').getTime(), value: 500 },
-            { time: new Date().getTime() - MS_DAY * 8, value: 1000 },
-            { time: new Date(), value: 1000 },
-        ]
-
-        const dayNames = [
-            "Sunday",
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-        ];
-        expect(getMostHydratedDayOfWeek(data)).toEqual({ name: dayNames[new Date().getDay()], value: 1000 });
     });
-    test('no data in day of week', () => {
+    test("mixed weeks", () => {
         const data = [
+            { time: new Date("2024-07-01T00:00:00Z").getTime(), value: 1000 }, // Monday
+            { time: new Date("2024-07-02T00:00:00Z").getTime(), value: 1000 }, // Tues
+            { time: new Date("2024-07-03T00:00:00Z").getTime(), value: 2000 }, // Weds
+            { time: new Date("2024-07-04T00:00:00Z").getTime(), value: 1999 }, // Thurs
+            { time: new Date("2024-07-05T00:00:00Z").getTime(), value: 1 }, // Friday
+        ];
 
-        ]
         const dayNames = [
             "Sunday",
             "Monday",
@@ -117,7 +102,26 @@ describe("insights", () => {
             "Friday",
             "Saturday",
         ];
-        expect(getMostHydratedDayOfWeek(data)).toEqual({ name: "No data", value: 0 });
+        expect(getMostHydratedDayOfWeek(data)).toEqual({
+            name: "Wednesday",
+            value: 2000,
+        });
+    });
+    test("no data in day of week", () => {
+        const data = [];
+        const dayNames = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+        ];
+        expect(getMostHydratedDayOfWeek(data)).toEqual({
+            name: "No data",
+            value: 0,
+        });
     });
     test("average hydration month comparison", () => {
         const testData = [];
@@ -139,7 +143,7 @@ describe("insights", () => {
             testData.push({
                 x: newTS,
                 y: 1500,
-            })
+            });
         }
         for (let i = 0; i < 31; i++) {
             const newTS = startOfPrevMonth + i * MS_DAY;
@@ -147,16 +151,16 @@ describe("insights", () => {
             testData.push({
                 x: newTS,
                 y: 2000,
-            })
+            });
         }
-        const [firstAvg, nextAvg] = averageHydrationMonthComparison(testData)
+        const [firstAvg, nextAvg] = averageHydrationMonthComparison(testData);
         expect(firstAvg).toEqual(1500);
         expect(nextAvg).toEqual(2000);
-    })
+    });
     test("passing undefined to average hydration month comparison", () => {
         const testData = [];
         const [firstAvg, nextAvg] = averageHydrationMonthComparison(testData);
         expect(firstAvg).toEqual(0);
         expect(nextAvg).toEqual(0);
-    })
+    });
 });
