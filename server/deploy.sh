773d8bfc2c28b1b771ecd3d9abe1be61dd8b900d
Hamish Partington <hamishpartington25@gmail.com>
2024-07-11 18:17:34 +0100

    update

--- server/deploy.sh
+++ server/deploy.sh
@@ -3,6 +3,8 @@
 SSH_USER="ec2-user"
 SSH_SERVER="ec2-18-133-247-202.eu-west-2.compute.amazonaws.com"
 
+file=$1
+
 zip -r deploy.zip app/ Dockerfile Makefile requirements.txt .env
 
 scp -i "$file" deploy.zip ${SSH_USER}@${SSH_SERVER}:~
