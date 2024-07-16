2f04e5f62d8321101405b6db7e50c6b80dfdb570
Hamish Partington <hamishpartington25@gmail.com>
2024-07-16 18:20:46 +0100

    Move endpoints to routers

---
+++ server/app/routers/jug.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends, HTTPException
+from pony.orm.core import db_session, select
+
+from ..api import login_and_get_session, get_jug_latest, get_hydration_events
+from ..auth import auth_user
+from ..models import User, Jug
+from ..schemas import UpdateJugForm
+from ..services import user_exists, update_jug_name_s, get_users_jugs
+
+router = APIRouter(
+    prefix="/jug",
+    tags=["jug"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@app.post("/update-name")
+async def update_jug_name(form: UpdateJugForm, user_id: str = Depends(auth_user)):
+    with db_session:
+        jugs = get_users_jugs(user_id)
+
+        jug = Jug.get(smart_hydration_id=form.jugId)
+
+        if jug not in jugs:
+            raise HTTPException(status_code=401, detail='Unauthorized')
+
+        update_jug_name_s(form.jugId, form.name)
