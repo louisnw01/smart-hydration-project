082724eb292ddad0417433c88454822ca4a993be
chriskerrc <chriskerrc@gmail.com>
2024-07-17 19:28:55 +0100

    Add text field and button to Create/Join community modals

--- smart-hydration-app/app/(main)/(modals)/create-community-modal.tsx
+++ smart-hydration-app/app/(main)/(modals)/create-community-modal.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { View, Text, SectionList, Pressable } from "react-native";
+import { View, Text, SectionList, Pressable, TextInput } from "react-native";
 import { useAtomValue } from "jotai";
 import { getAllJugsQAtom, linkJugToUserMAtom } from "@/atom/query";
 import { useNavigation } from "expo-router";
-import Loading from "@/components/common/loading";
+import Button from "@/components/common/button";
 
 export default function CreateCommunityModal() {
     const navigation = useNavigation();
@@ -13,13 +13,22 @@ export default function CreateCommunityModal() {
     };
 
     return (
-        <View className="flex gap-6 h-full pb-20">
-            <View className="flex gap-6 h-full pb-20">
-                <View className="flex flex-row justify-center items-center">
-                    <Text className="dark:text-white text-2xl">
-                        Placeholder message.
-                    </Text>
-                </View>
+        <View className="mt-8 flex gap-6">
+            <View className="flex flex-row justify-center items-center">
+                <Text className="dark:text-white text-2xl">
+                    Enter a community name:
+                </Text>
+            </View>
+            <View className="flex flex-row justify-center items-center">
+                <TextInput
+                    placeholder={`Community name (required)`}
+                    className="bg-gray-200 h-14 placeholder-black text-xl rounded-xl px-3"
+                    textContentType="name"
+                    returnKeyType="done"
+                />
+            </View>
+            <View className="flex flex-row justify-center items-center">
+                <Button text="Submit" href="community"></Button>
             </View>
         </View>
     );
