dc74f69b2cbfd72fe61ee8287a37feb485fad102
chriskerrc <chriskerrc@gmail.com>
2024-07-18 15:43:23 +0100

    Create community modal: check if community name entered before Submit

--- smart-hydration-app/app/(main)/(modals)/create-community-modal.tsx
+++ smart-hydration-app/app/(main)/(modals)/create-community-modal.tsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
-import { View, Text, SectionList, Pressable, TextInput } from "react-native";
-import { useAtomValue } from "jotai";
-import { getAllJugsQAtom, linkJugToUserMAtom } from "@/atom/query";
+import { View, Text, Pressable, TextInput } from "react-native";
+import { createCommunityMAtom } from "@/atom/query/community";
 import { useNavigation } from "expo-router";
-import Button from "@/components/common/button";
+import { useAtomValue } from "jotai";
 
 export default function CreateCommunityModal() {
     const navigation = useNavigation();
+    const [communityName, setName] = useState('');
+    const {mutate} = useAtomValue(createCommunityMAtom);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
 
     const handlePress = () => {
-        navigation.goBack();
+        //to do: check if community exists before allowing creation 
+        if(communityName !== ''){
+            mutate({ name: communityName });
+            navigation.goBack();
+        }
+        else{
+            setShowErrorMessage(true);
+        }
     };
 
     return (
@@ -23,12 +32,28 @@ export default function CreateCommunityModal() {
                 <TextInput
                     placeholder={`Community name (required)`}
                     className="bg-gray-200 h-14 placeholder-black text-xl rounded-xl px-3"
+                    onChangeText={(val) => { 
+                        setName(val); 
+                        setShowErrorMessage(false); 
+                    }}
                     textContentType="name"
                     returnKeyType="done"
                 />
             </View>
+            {showErrorMessage && (
             <View className="flex flex-row justify-center items-center">
-                <Button text="Submit" href="community"></Button>
+                <Text className="dark:text-white text-2xl">
+                    You must enter a community name
+                </Text>
+            </View>
+            )}
+            <View className="flex flex-row justify-center items-center">
+                <Pressable
+                    onPress={handlePress}
+                    className="bg-blue px-4 py-2 rounded-xl mt-10"
+                ><Text className="text-2xl font-semibold text-white">
+                        Submit
+                    </Text></Pressable>
             </View>
         </View>
     );
