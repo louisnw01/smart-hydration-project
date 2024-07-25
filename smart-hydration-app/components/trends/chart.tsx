02852988abb3c875a1432228644628fd1c6c59da
louisnw <louisnw@icloud.com>
2024-07-25 19:27:09 +0100

    implement tooltip, scrolling chart, y axis

---
+++ smart-hydration-app/components/trends/chart.tsx
@@ -0,0 +1,207 @@
+import { CartesianChart, Bar, useChartPressState } from "victory-native";
+import SFPro from "@/assets/fonts/SF-Pro-Display-Regular.otf";
+import { useFont } from "@shopify/react-native-skia";
+import { atom, useAtomValue, useSetAtom } from "jotai";
+import { chartTimeWindowAtom } from "@/atom/nav";
+import { formattedDataAtom } from "@/util/trends";
+import useColorPalette from "@/util/palette";
+import { useEffect, useMemo, useRef, useState } from "react";
+import {
+  ActivityIndicator,
+  View,
+  Text,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+import ToolTip from "@/components/trends/tooltip";
+import ChartAxis from "./axis";
+import Animated, {
+  useAnimatedRef,
+  useScrollViewOffset,
+  useSharedValue,
+  scrollTo,
+  useDerivedValue,
+  runOnUI,
+  measure,
+  runOnJS,
+} from "react-native-reanimated";
+
+const tickCountDivisorMap = {
+  D: 4,
+  W: 1,
+  M: 2,
+  Y: 5,
+};
+
+const chartWidthDivisorMap = {
+  D: 24,
+  W: 7,
+  M: 24,
+  Y: 20,
+};
+
+const tickFormatMap: { [key: string]: (t: Date) => string } = {
+  D: (t) => {
+    const hours = t.getHours();
+    const period = hours >= 12 ? "am" : "pm";
+    const formattedHour = hours % 12 || 12;
+    return `${formattedHour}${period}`;
+  },
+  W: (t) => ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][t.getDay()],
+  M: (t) => t.getDate().toString(),
+  Y: (t) =>
+    [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ][t.getMonth()],
+};
+
+function formatDateToDayMonth(date) {
+  const daysSuffixes = ["th", "st", "nd", "rd"];
+  const day = date.getUTCDate();
+  const suffix =
+    day % 10 <= 3 && (day < 11 || day > 13)
+      ? daysSuffixes[day % 10]
+      : daysSuffixes[0];
+  const month = date.toLocaleString("default", { month: "short" });
+  return `${day}${suffix} ${month}`;
+}
+
+export const canvasInfoAtom = atom([null, null]);
+
+export default function TrendsChart() {
+  const font = useFont(SFPro);
+  const timeframe = useAtomValue(chartTimeWindowAtom);
+  const data = useAtomValue(formattedDataAtom);
+  const { state, isActive } = useChartPressState({ x: "", y: { y: 0 } });
+  const palette = useColorPalette();
+  const setCanvasInfo = useSetAtom(canvasInfoAtom);
+
+  const scrollRef = useAnimatedRef();
+  const scrollPosition = useScrollViewOffset(scrollRef);
+  const scrollViewWidth = useSharedValue(0);
+
+  const memoedData = useMemo(
+    () =>
+      data.map((val) => ({
+        x: tickFormatMap[timeframe](new Date(val.x)),
+        y: val.y,
+      })),
+    [data]
+  );
+
+  if (!data || data.length === 0) {
+    return (
+      <View className="h-full justify-center text-center">
+        <ActivityIndicator />
+        <Text className="text-center dark:text-white">
+          Loading Analytics, Please Wait...
+        </Text>
+      </View>
+    );
+  }
+
+  const zeroCount = memoedData.reduce(
+    (curr, row) => curr + (row.y == 0 ? 1 : 0),
+    0
+  );
+  const hasNoData = zeroCount == memoedData.length;
+
+  if (hasNoData) {
+    return (
+      <View className="h-[300px] justify-center items-center">
+        <Text className="dark:text-white">No data for this period.</Text>
+      </View>
+    );
+  }
+  const chartWidthPct = (100 * data.length) / chartWidthDivisorMap[timeframe];
+
+  const scrollWidth = useSharedValue(0);
+
+  useEffect(() => {
+    console.log(1);
+    scrollWidth.value += 100;
+  }, [timeframe]);
+
+  return (
+    <View className="flex-row">
+      <Animated.ScrollView
+        showsHorizontalScrollIndicator={false}
+        ref={scrollRef}
+        onLayout={(e) => console.log("EHAJR", e)}
+        horizontal
+        contentContainerClassName={`h-[300px] w-[${chartWidthPct.toFixed(0)}%]`}
+        onScroll={(e) => (scrollPosition.value = e.nativeEvent.contentOffset.x)}
+      >
+        <CartesianChart
+          data={hasNoData ? [] : memoedData.toReversed()}
+          xKey="x"
+          yKeys={["y"]}
+          chartPressState={state}
+          domain={{ y: [0] }}
+          domainPadding={{ bottom: 0, left: 30, top: 100, right: 40 }}
+          axisOptions={{
+            font: font,
+            // axisSide: { x: "bottom", y: "right" },
+            tickCount: {
+              x: memoedData.length / tickCountDivisorMap[timeframe],
+              // y: 4,
+              y: 0,
+            },
+            labelColor: palette.fg,
+            lineColor: palette.border,
+          }}
+        >
+          {({ points, chartBounds }) => {
+            setCanvasInfo([points.y, chartBounds]);
+            return (
+              <>
+                <Bar
+                  points={points.y}
+                  chartBounds={chartBounds}
+                  // color="red"
+                  // barWidth={barWidthMap[timeframe]}
+                  color="#5cb5e1"
+                  roundedCorners={{ topLeft: 4, topRight: 4 }}
+                  // innerPadding={0.55}
+                  animate={{ type: "timing" }}
+                />
+
+                <ToolTip
+                  isActive={isActive}
+                  x={state.x}
+                  y={state.y.y}
+                  points={points.y}
+                  timeframe={timeframe}
+                  scrollPosition={scrollPosition}
+                  chartBounds={chartBounds}
+                />
+              </>
+            );
+          }}
+          {/*
+                <VictoryLine
+                    data={
+                        memoedData?.map((row) => ({ x: row.x, y: 2200 })) ?? []
+                    }
+                /> */}
+        </CartesianChart>
+      </Animated.ScrollView>
+      <ChartAxis />
+      <Text className="absolute top-3 right-10 dark:text-white">
+        {formatDateToDayMonth(new Date(data[data.length - 1].x))}-
+        {formatDateToDayMonth(new Date(data[0].x))}
+      </Text>
+    </View>
+  );
+}
