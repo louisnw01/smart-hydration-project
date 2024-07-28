5d9f18d54e16e53995d42a9349982d09f7b0ce21
louisnw01 <97458988+louisnw01@users.noreply.github.com>
2024-07-28 17:02:35 +0100

    fix merge bugs

--- smart-hydration-app/components/common/button.tsx
+++ smart-hydration-app/components/common/button.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "expo-router";
 import { ReactNode, useState } from "react";
-import { Pressable, Text, PressableProps} from "react-native";
+import { Pressable, PressableProps, Text } from "react-native";
 
 interface ButtonProps extends PressableProps {
     text: string;
@@ -16,27 +16,34 @@ export default function StyledButton(props: ButtonProps) {
     const [touched, setTouched] = useState(false);
 
     props.textClass +=
-        props.textClass && !props.textClass.includes("dark:text") ? " dark:text-white" : "";
+        props.textClass && !props.textClass.includes("dark:text")
+            ? " dark:text-white"
+            : "";
 
-    const buttonColors = touched || props.disabled
-        ? "bg-gray-300 dark:bg-neutral-700"
-        : "bg-gray-200 dark:bg-neutral-800";
+    const buttonColors =
+        touched || props.disabled
+            ? "bg-gray-300 dark:bg-neutral-700"
+            : "bg-gray-200 dark:bg-neutral-800";
 
     const buttonClass = !props.buttonClass
         ? buttonColors
         : !props.buttonClass.includes("bg-")
-            ? (props.buttonClass += " " + buttonColors)
-            : props.buttonClass;
+          ? (props.buttonClass += " " + buttonColors)
+          : props.buttonClass;
 
     const finalButtonClass =
         "flex-row gap-[3px] px-4 py-2 rounded-3xl " + buttonClass;
 
     return (
         <Pressable
-            style={style}
+            style={props.style}
             className={finalButtonClass}
             onPress={() =>
-                props.onPress ? props.onPress : props.href ? router.navigate(props.href) : undefined
+                props.onPress
+                    ? props.onPress
+                    : props.href
+                      ? router.navigate(props.href)
+                      : undefined
             }
             onTouchStart={() => setTouched(true)}
             onTouchEnd={() => setTouched(false)}
