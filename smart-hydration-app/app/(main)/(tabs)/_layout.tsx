e64fcb1126dd0f042ba2b4e09ea9e38a0f250c7c
louisnw01 <97458988+louisnw01@users.noreply.github.com>
2024-07-29 11:54:09 +0100

    fix React import

--- smart-hydration-app/app/(main)/(tabs)/_layout.tsx
+++ smart-hydration-app/app/(main)/(tabs)/_layout.tsx
@@ -9,9 +9,7 @@ import {
     MaterialIcons,
 } from "@expo/vector-icons";
 
-import React from "react";
 import { Link, router, Tabs } from "expo-router";
-import React from "react";
 import { Pressable } from "react-native";
 
 export default function TabLayout() {
@@ -110,7 +108,11 @@ export default function TabLayout() {
                 options={{
                     title: "Community",
                     tabBarIcon: ({ color }) => (
-                        <FontAwesome6 name="people-group" size={24} color={color} />
+                        <FontAwesome6
+                            name="people-group"
+                            size={24}
+                            color={color}
+                        />
                     ),
                     headerRight: () => (
                         <Link className="px-5" href="add-member-modal">
