0c8905e54f9943200d362ff9728614eee8de12b0
louisnw01 <97458988+louisnw01@users.noreply.github.com>
2024-07-29 16:46:29 +0100

    general consistency changes, use droplet icon from smart hydration

--- smart-hydration-app/components/common/page-wrapper.tsx
+++ smart-hydration-app/components/common/page-wrapper.tsx
@@ -1,5 +1,7 @@
-import { View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useQueryRefetch } from "@/util/query-refetch";
+import { Atom } from "jotai";
+import { AtomWithQueryResult } from "jotai-tanstack-query";
+import { RefreshControl, ScrollView, View } from "react-native";
 import Loading from "./loading";
 
 export default function PageWrapper({
@@ -11,17 +13,46 @@ export default function PageWrapper({
     children: JSX.Element | JSX.Element[];
     isLoading?: boolean;
     message?: string;
+    className?: string;
 }) {
-    const insets = useSafeAreaInsets();
     if (isLoading) return <Loading isLoading message={message} />;
     return (
-        <View
-            className="flex flex-1 dark:bg-black"
-            style={{
-                paddingTop: insets.top + 30,
-            }}
-        >
+        <View className={`flex flex-1 dark:bg-black ${className}`}>
             {children}
         </View>
+    );
+}
+
+export function ScrollPageWrapper({
+    children,
+    isLoading,
+    message,
+    className,
+    queryRefreshAtom,
+}: {
+    children: JSX.Element | JSX.Element[];
+    isLoading?: boolean;
+    message?: string;
+    className?: string;
+    queryRefreshAtom: Atom<AtomWithQueryResult>;
+}) {
+    const { isRefreshing, handleRefresh } = queryRefreshAtom
+        ? useQueryRefetch(queryRefreshAtom)
+        : { isRefreshing: undefined, handleRefresh: undefined };
+
+    if (isLoading) return <Loading isLoading message={message} />;
+
+    return (
+        <ScrollView
+            contentContainerClassName={className}
+            refreshControl={
+                <RefreshControl
+                    refreshing={isRefreshing || false}
+                    onRefresh={handleRefresh}
+                />
+            }
+        >
+            {children}
+        </ScrollView>
     );
 }
