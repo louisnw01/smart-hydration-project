08ef08de960566325a975e76a8277498f23c3179
louisnw01 <97458988+louisnw01@users.noreply.github.com>
2024-07-30 15:43:42 +0100

    community style changes

--- smart-hydration-app/components/common/header.tsx
+++ smart-hydration-app/components/common/header.tsx
@@ -1,9 +1,11 @@
+import { communityNameAtom } from "@/atom/query/community";
 import useColorPalette from "@/util/palette";
+import { useAtomValue } from "jotai";
 import { Text } from "react-native";
 
 export default function PageHeader({ children }: { children: string }) {
     const palette = useColorPalette();
-
+    const communityName = useAtomValue(communityNameAtom);
     return (
         <Text
             className="font-semibold text-3xl ml-1"
@@ -11,7 +13,11 @@ export default function PageHeader({ children }: { children: string }) {
                 color: palette.fg,
             }}
         >
-            {children === "Home" ? "smart hydration" : children}
+            {children === "Home"
+                ? "smart hydration"
+                : children == "Community"
+                  ? communityName
+                  : children}
         </Text>
     );
 }
