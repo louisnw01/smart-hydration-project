eb446213525af49744021d29f59bf82f3034b6fa
louisnw01 <97458988+louisnw01@users.noreply.github.com>
2024-07-31 14:42:33 +0100

    finish tooltip + scrolling chart

--- smart-hydration-app/components/trends/tooltip.tsx
+++ smart-hydration-app/components/trends/tooltip.tsx
@@ -1,143 +1,144 @@
-import { Line, RoundedRect, Text, useFont } from "@shopify/react-native-skia";
-import {
-  SharedValue,
-  useAnimatedReaction,
-  useDerivedValue,
-} from "react-native-reanimated";
 import SFPro from "@/assets/fonts/SF-Pro-Display-Regular.otf";
 import useColorPalette from "@/util/palette";
-import { atom, useAtom, useAtomValue } from "jotai";
-import { colorSchemeAtom } from "@/atom/user";
-import { Dimensions, useColorScheme } from "react-native";
-import { formattedDataAtom } from "@/util/trends";
-import { useEffect, useMemo, useState } from "react";
-import { chartTimeWindowAtom } from "@/atom/nav";
+import { Line, RoundedRect, Text, useFont } from "@shopify/react-native-skia";
+import { useEffect, useState } from "react";
+import { useColorScheme } from "react-native";
+import { useDerivedValue } from "react-native-reanimated";
 
 interface ValPosSkia {
-  value: any;
-  position: any;
+    value: any;
+    position: any;
 }
-
-const screenWidth = Dimensions.get("screen").width;
 
 const BOX_Y = 10;
 
 export default function ToolTip({
-  isActive,
-  x,
-  y,
-  points,
-  timeframe,
-  scrollPosition,
-  chartBounds,
+    isActive,
+    x,
+    y,
+    points,
+    timeframe,
+    scrollPosition,
+    chartBounds,
+    data,
 }: {
-  isActive: boolean;
-  x: ValPosSkia;
-  y: ValPosSkia;
+    isActive: boolean;
+    x: ValPosSkia;
+    y: ValPosSkia;
 }) {
-  const scheme = useColorScheme();
-  const palette = useColorPalette();
-  const fontLarge = useFont(SFPro, 30);
-  const fontSmall = useFont(SFPro, 15);
-  const fontMedium = useFont(SFPro, 23);
+    const scheme = useColorScheme();
+    const palette = useColorPalette();
+    const fontLarge = useFont(SFPro, 30);
+    const fontSmall = useFont(SFPro, 15);
 
-  const data = useAtomValue(formattedDataAtom);
+    // const data = useAtomValue(formattedDataAtom);
 
-  const memoedData = useMemo(() => data.toReversed(), [data]);
+    const memoedData = data.toReversed();
+    const [clickedDataIndex, setClickedDataIndex] = useState(null);
 
-  const [clickedDataIndex, setClickedDataIndex] = useState(null);
+    const [shown, setShown] = useState(false);
+    useEffect(() => {
+        if (isActive) setShown(!shown);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isActive]);
 
-  const [shown, setShown] = useState(false);
-  useEffect(() => {
-    if (isActive) setShown(!shown);
-  }, [isActive]);
+    useEffect(() => {
+        setClickedDataIndex(
+            points?.findIndex((row) => row.x == x.position.value),
+        );
+        setShown(true);
 
-  useEffect(() => {
-    setClickedDataIndex(points?.findIndex((row) => row.y == y.position.value));
-    setShown(true);
-  }, [x.position.value]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [points, timeframe, data]);
 
-  useEffect(() => {
-    setShown(false);
-  }, [timeframe]);
+    useEffect(() => {
+        setShown(false);
+    }, [timeframe]);
 
-  useEffect(() => {
-    if (scrollPosition.value > x.position.value) {
-      setShown(false);
-    }
-  }, [scrollPosition.value]);
+    useEffect(() => {
+        if (scrollPosition.value > x.position.value) {
+            setShown(false);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [scrollPosition.value]);
 
-  const [width, height] = [200, 80];
+    const [width, height] = [200, 80];
 
-  const xPos = useDerivedValue(() => {
-    const width = 200;
+    const xPos = useDerivedValue(() => {
+        const width = 200;
 
-    if (x.position.value - width / 2 < scrollPosition.value) {
-      if (x.position.value - 20 < scrollPosition.value) {
-        return x.position.value - 20;
-      }
-      return scrollPosition.value;
-    } else if (x.position.value - scrollPosition.value > 215) {
-      if (x.position.value - scrollPosition.value > 265) {
-        return x.position.value - 150;
-      }
-      return scrollPosition.value + 215 - width / 2;
-    } else {
-      return x.position.value - width / 2;
-    }
-  }, [scrollPosition, x.position.value]);
+        if (x.position.value - width / 2 < scrollPosition.value) {
+            if (x.position.value - 20 < scrollPosition.value) {
+                return x.position.value - 20;
+            }
+            return scrollPosition.value;
+        } else if (x.position.value - scrollPosition.value > 215) {
+            if (x.position.value - scrollPosition.value > 265) {
+                return x.position.value - 150;
+            }
+            return scrollPosition.value + 215 - width / 2;
+        } else {
+            return x.position.value - width / 2;
+        }
+    }, [scrollPosition, x.position.value]);
 
-  const textXPos = useDerivedValue(() => xPos.value + 20, [xPos]);
-  if (!shown || !memoedData || !y.value.value) return null;
+    const textXPos = useDerivedValue(() => xPos.value + 20, [xPos]);
+    if (!shown || !memoedData || !y.value.value) return null;
 
-  if (
-    memoedData[clickedDataIndex] == null ||
-    memoedData[clickedDataIndex] == undefined
-  )
-    return null;
+    if (
+        memoedData[clickedDataIndex] == null ||
+        memoedData[clickedDataIndex] == undefined
+    )
+        return null;
 
-  const realTime = new Date(memoedData[clickedDataIndex].x);
+    const realTime = new Date(memoedData[clickedDataIndex].x);
 
-  return (
-    <>
-      <RoundedRect
-        x={xPos}
-        y={BOX_Y}
-        r={10}
-        width={width}
-        height={height}
-        color={scheme == "dark" ? "rgb(35, 35, 35)" : "rgb(230, 230, 230)"}
-        strokeWidth={3}
-      />
-      <Text
-        x={textXPos}
-        y={BOX_Y + 20}
-        text="Total"
-        font={fontSmall}
-        antiAlias={true}
-        color={palette.fg}
-      />
-      <Text
-        x={textXPos}
-        y={BOX_Y + 50}
-        text={`${y.value.value.toFixed(0)}ml`}
-        font={fontLarge}
-        color={palette.fg}
-      />
-      <Text
-        x={textXPos}
-        y={BOX_Y + 70}
-        text={realTime.toLocaleString(undefined, { dateStyle: "medium" })}
-        font={fontSmall}
-        color={palette.fg}
-      />
+    return (
+        <>
+            <RoundedRect
+                x={xPos}
+                y={BOX_Y}
+                r={10}
+                width={width}
+                height={height}
+                color={
+                    scheme == "dark" ? "rgb(35, 35, 35)" : "rgb(230, 230, 230)"
+                }
+                strokeWidth={3}
+            />
+            <Text
+                x={textXPos}
+                y={BOX_Y + 20}
+                text="Total"
+                font={fontSmall}
+                antiAlias={true}
+                color={palette.fg}
+            />
+            <Text
+                x={textXPos}
+                y={BOX_Y + 50}
+                text={`${y.value.value.toFixed(0)}ml`}
+                font={fontLarge}
+                color={palette.fg}
+            />
+            <Text
+                x={textXPos}
+                y={BOX_Y + 70}
+                text={realTime.toLocaleString(undefined, {
+                    dateStyle: timeframe != "Y" ? "medium" : undefined,
+                    timeStyle: timeframe == "D" ? "medium" : undefined,
+                    month: timeframe == "Y" ? "long" : undefined,
+                })}
+                font={fontSmall}
+                color={palette.fg}
+            />
 
-      <Line
-        p1={{ x: x.position.value, y: BOX_Y + height }}
-        p2={{ x: x.position.value, y: y.position.value }}
-        strokeWidth={3}
-        color={palette.border}
-      />
-    </>
-  );
+            <Line
+                p1={{ x: x.position.value, y: BOX_Y + height }}
+                p2={{ x: x.position.value, y: y.position.value }}
+                strokeWidth={3}
+                color={palette.border}
+            />
+        </>
+    );
 }
