edc7cd14367fef4a1dc5175aef636bc66a434408
Hamish Partington <hamishpartington25@gmail.com>
2024-08-06 11:33:52 +0100

    duplication

--- smart-hydration-app/atom/query.ts
+++ smart-hydration-app/atom/query.ts
@@ -559,19 +559,12 @@ export const addTagsPatientMAtom = atomWithMutation((get) => ({
     mutationFn: async () => {
         const member = get(selectedMemberAtom);
 
-        const formData: { memberID: number; memberTags: string[] } = {
-            memberID: member.id as number,
-            memberTags: member.tags as string[],
-        };
-        console.log("member id:", member.id);
-        console.log("member tags:", member.tags);
         const formData: {memberID: number, memberTags: TagInfo[]} = 
             {
                 memberID: member.id as number,
                 memberTags: member.tags as TagInfo[],
             };
-            console.log("member id:", member.id);
-            console.log("member tags:", member.tags);
+            
         const token = get(authTokenAtom);
         const response = await request(ENDPOINTS.ADD_TAGS_PATIENT, {
             method: "post",
