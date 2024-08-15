d4962d2eabf1fe699d56d1ad358570ceb00598e0
louisnw01 <97458988+louisnw01@users.noreply.github.com>
2024-08-15 12:05:25 +0100

    jug simulator

---
+++ sh-simulator/src/app/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const LOCAL_SERVER_URL = "http://localhost:8085/simulator";
+
+export default function Home() {
+    const [dbValue, setDbValue] = useState(0);
+    const [value, setValue] = useState(0);
+
+    const fetchWaterLevel = () => {
+        fetch(LOCAL_SERVER_URL + "/latest")
+            .then((res) => res.json())
+            .then((data) => {
+                setValue(data);
+                setDbValue(data);
+            });
+    };
+
+    useEffect(() => {
+        fetchWaterLevel();
+    }, []);
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+
+    const updateValue = async () => {
+        const res = await fetch(LOCAL_SERVER_URL + "/update-level", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ level: parseInt(value) }),
+        });
+
+        if (res.ok) {
+            setDbValue(value);
+        }
+    };
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center gap-5">
+            <div className="">Jug simulator</div>
+            <p>Current value: {dbValue}ml</p>
+            <p>Value: {value}ml</p>
+            <div className="w-72">
+                <input
+                    type="range"
+                    min="0"
+                    max="1100"
+                    value={value}
+                    onChange={handleChange}
+                    className="appearance-none bg-gray-200 focus:outline-none h-1 w-full rounded-full"
+                />
+            </div>
+            <button
+                className="bg-blue-500 px-4 py-2 rounded-lg mt-10"
+                onTouchStart={() => updateValue()}
+                onClick={() => updateValue()}
+            >
+                Update
+            </button>
+        </main>
+    );
+}
