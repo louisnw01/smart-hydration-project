7422789ee078882b8ac9aeee5ec93d17aa58dff3
ijgs02 <isaacjgscott@gmail.com>
2024-08-23 12:11:03 +0100

    fix logo offscreen for different device sizes

--- smart-hydration-app/components/onboarding/onboarding-header.tsx
+++ smart-hydration-app/components/onboarding/onboarding-header.tsx
@@ -1,8 +1,12 @@
+import { Dimensions, Keyboard } from "react-native";
 import { Text } from "react-native";
 
 export default function OnboardingHeader({ text }: { text: string }) {
+    const windowHeight = Dimensions.get("screen").height;
     return (
-        <Text className="font-bold text-4xl dark:text-white w-full text-center pb-20">
+        <Text
+            className={`font-bold text-4xl dark:text-white w-full text-center ${windowHeight > 667 ? "top-6" : ""}`}
+        >
             {text}
         </Text>
     );
