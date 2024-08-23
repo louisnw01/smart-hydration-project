d61c541b53a3b0e03ebd1ba7e0cdb38b13715751
louisnw01 <97458988+louisnw01@users.noreply.github.com>
2024-08-23 13:44:50 +0100

    fix expo linking bug by removing notification linking code

--- smart-hydration-app/util/auth.ts
+++ smart-hydration-app/util/auth.ts
@@ -1,35 +1,31 @@
-import { emailIsVerifiedAtom } from "@/atom/user";
-import { getItemAsync } from "expo-secure-store";
-import { useSetAtom } from "jotai";
-import { useEffect, useState } from "react";
+import { authTokenAtom, emailIsVerifiedAtom } from "@/atom/user";
+import { useAtomValue, useSetAtom } from "jotai";
+import { atomWithQuery } from "jotai-tanstack-query";
+import { useEffect } from "react";
 import { request } from "./fetch";
 
+const checkTokenQAtom = atomWithQuery((get) => ({
+    queryKey: ["check-token", get(authTokenAtom)],
+    queryFn: async ({ queryKey: [, token] }) => {
+        console.log("checkTokenQueryFunctionRan!");
+        const response = await request("/user/check-token", {
+            auth: token as string,
+            method: "post",
+        });
+        return response.status != 403;
+    },
+    enabled: !!get(authTokenAtom),
+    placeholderData: undefined,
+}));
+
 export default function useSession() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [isSuccess, setIsSuccess] = useState(false);
-    const [isEmailVerified, setIsEmailVerified] = useState(true);
+    const { isFetching, isSuccess, data } = useAtomValue(checkTokenQAtom);
+
     const setEmailVerifiedAtom = useSetAtom(emailIsVerifiedAtom);
 
-    const getTokenFromStorage = async () => {
-        const rawToken = await getItemAsync("auth-token");
-        const authToken = rawToken ? JSON.parse(rawToken) : null;
-        if (!authToken) {
-            setIsSuccess(false);
-            setIsLoading(false);
-            return;
-        }
-        const result = await request("/user/check-token", {
-            method: "post",
-            auth: authToken,
-        });
-        setIsSuccess(result.ok);
-        setIsEmailVerified(result.status != 403);
-        setEmailVerifiedAtom(result.status != 403);
-        setIsLoading(false);
-    };
-
     useEffect(() => {
-        getTokenFromStorage();
-    }, []);
-    return { isLoading, isSuccess, isEmailVerified };
+        if (data != undefined) setEmailVerifiedAtom(data);
+    }, [data]);
+
+    return { isLoading: isFetching, isSuccess, isEmailVerified: data };
 }
