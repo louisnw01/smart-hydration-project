84b0a8a1f42dafc889911c3f2c000c88fcea25f8
chriskerrc <chriskerrc@gmail.com>
2024-08-29 14:29:42 +0100

    Adjust text to fluids, not just water

--- smart-hydration-app/app/(main)/custom/add-drink-modal.tsx
+++ smart-hydration-app/app/(main)/custom/add-drink-modal.tsx
@@ -5,7 +5,7 @@ export default function AddDrinkModal() {
     return (
         <View>
             <Text className="dark:text-white font-bold text-2xl mt-4 mx-6 text-center">
-                Select a volume of water
+                Containers represent a volume of any fluid
             </Text>
             <View className="top-5">
                 <AddDrinkPane />
